d11d71ee2e58defb0dc1dcc8a9b0e00e8e72c6c1
Alvaro Tolentino <alvarotolentinou@gmail.com>
2021-12-08 17:43:09 -0500

    Add Search component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,24 +1,23 @@
 import Head from 'next/head';
-import {Box} from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
-const Layout = ({children}) => (
-    <>
-        <Head>
-            <title>Real Estate</title>
-        </Head>
-        <Box maxWidth='1280px' m='auto'>
-            <header>
-                <Navbar />
-            </header>
-            <main>
-                {children}
-            </main>
-            <footer>
-                Footer
-            </footer>
-        </Box>
-    </>
-)
+const Layout = ({ children }) => (
+  <>
+    <Head>
+      <title>Real Estate</title>
+    </Head>
+    <Box maxWidth='1280px' m='auto'>
+      <header>
+        <Navbar />
+      </header>
+      <main>{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </Box>
+  </>
+);
 
-export default Layout
+export default Layout;
